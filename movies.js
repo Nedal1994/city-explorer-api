3a22c5866f4ade222f39bedc7c7868e94ac136ea
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-09-11 20:42:52 +0300

    ok

--- movies.js
+++ movies.js
@@ -36,7 +36,7 @@ function moviesHandler(req,res)
             this.popularity=item.popularity
             this.vote_average=item.vote_average
             this.vote_count=item.vote_count
-            this.image_url=`https://image.tmdb.org/t/p/w500/${item.poster_path}`
+            this.poster_path=`https://image.tmdb.org/t/p/w500${item.poster_path}`
         }
     }
 }
